d7dc90a6c6064d17401f9ea6cfc3d05304ff5ca9
Ícaro Motta <icaromotta@unifei.edu.br>
2023-10-30 21:38:53 -0300

    feat: adicionando conexão com o banco de dados

--- routes/index.go
+++ routes/index.go
@@ -1,8 +1,11 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
-func SetRoutes(router *gin.Engine) {
+func SetRoutes(router *gin.Engine, db *gorm.DB) {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World!",

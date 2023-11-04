5b52e8821e2e2c46d780025723fce12674b9a186
Daniel <dani.dinheirow@gmail.com>
2023-11-04 13:13:43 -0300

    fix: corrigindo imports

--- routes/set_routes.go
+++ routes/set_routes.go
@@ -1,13 +1,13 @@
 package routes
 
 import (
-	"SociLinkApi/routes/auth"
-	routes "SociLinkApi/routes/user"
+	authRoutes "SociLinkApi/routes/auth"
+	userRoutes "SociLinkApi/routes/user"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func SetRoutes(router *gin.Engine, db *gorm.DB) {
-	routes.UserRoutes(router.Group("/users"), db)
-	routes.AuthRoutes(router.Group("/auth"), db)
+	userRoutes.UserRoutes(router.Group("/users"), db)
+	authRoutes.AuthRoutes(router.Group("/auth"), db)
 }

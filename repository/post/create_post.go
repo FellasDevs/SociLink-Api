87c6ad7cd8f5b38d6f552bda2f4588260d6d4499
Ícaro Motta <icaromotta@unifei.edu.br>
2023-11-07 12:05:24 -0300

    chore: adicionando cláusula de retorno às requisições de criação

--- repository/post/create_post.go
+++ repository/post/create_post.go
@@ -2,12 +2,13 @@ package postrepository
 
 import (
 	"SociLinkApi/models"
+	"gorm.io/gorm/clause"
 
 	"gorm.io/gorm"
 )
 
 func CreatePost(post *models.Post, db *gorm.DB) error {
-	result := db.Create(&post)
+	result := db.Clauses(clause.Returning{}).Create(&post)
 
 	return result.Error
 }
--- repository/user/create_user.go
+++ repository/user/create_user.go
@@ -3,10 +3,11 @@ package userrepository
 import (
 	"SociLinkApi/models"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 func CreateUser(user *models.User, db *gorm.DB) error {
-	result := db.Create(&user)
+	result := db.Clauses(clause.Returning{}).Create(&user)
 
 	return result.Error
 }

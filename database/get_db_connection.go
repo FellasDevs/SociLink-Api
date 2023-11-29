2f6e1afdcc118c07d37333407c48b38334a10816
Ícaro Motta <icaromotta@unifei.edu.br>
2023-11-28 22:17:59 -0300

    fix: removendo env que não funciona

--- database/get_db_connection.go
+++ database/get_db_connection.go
@@ -1,21 +1,11 @@
 package database
 
 import (
-	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
 )
 
 func GetDbConnection() (*gorm.DB, error) {
-	url := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=America/Sao_Paulo",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PW"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
-
-	return gorm.Open(postgres.Open(url), &gorm.Config{})
+	return gorm.Open(postgres.Open(os.Getenv("DB_STRING")), &gorm.Config{})
 }

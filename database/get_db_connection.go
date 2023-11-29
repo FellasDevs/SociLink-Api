d04685c4522eea3c1de40c13d7053c05ee842e41
Ícaro Motta <icaromotta@unifei.edu.br>
2023-11-28 22:56:29 -0300

    chore: adicionando print

--- database/get_db_connection.go
+++ database/get_db_connection.go
@@ -1,11 +1,13 @@
 package database
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"os"
 )
 
 func GetDbConnection() (*gorm.DB, error) {
+	fmt.Println(os.Getenv("DB_STRING"))
 	return gorm.Open(postgres.Open(os.Getenv("DB_STRING")), &gorm.Config{})
 }
